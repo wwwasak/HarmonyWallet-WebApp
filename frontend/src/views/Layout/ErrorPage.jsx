c15fc01c40fe9fb85a67c65830df653ad632cbf7
Xingyan Hu <xhu819@aucklanduni.ac.nz>
2024-04-10 23:55:35 +1200

    modified Error Page

--- frontend/src/views/Layout/ErrorPage.jsx
+++ frontend/src/views/Layout/ErrorPage.jsx
@@ -1,5 +1,5 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import { Box, Heading, Text, Flex, Center } from "@chakra-ui/react";
+import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
 import NavBar from "../Layout/components/NavBar";
 
 const ErrorPage = () => {
@@ -7,14 +7,30 @@ const ErrorPage = () => {
   return (
     <>
       <NavBar />
-      <Box padding={5}>
-        <Heading>Oops</Heading>
-        <Text>
-          {isRouteErrorResponse(error)
-            ? "This page does not exist."
-            : "An unexpected error occured."}
-        </Text>
-      </Box>
+      <Center minHeight="80vh">
+        <Box
+          padding={5}
+          bg="green.200"
+          width="40%"
+          height="250px"
+          borderRadius="lg"
+        >
+          <Flex
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            height="100%"
+          >
+            <Heading textAlign="center">Oops</Heading>
+            <Text mt={4} textAlign="center">
+              {isRouteErrorResponse(error)
+                ? "This page does not exist."
+                : "An unexpected error occurred."}
+            </Text>
+            <Link to="/">click here to return to home page</Link>
+          </Flex>
+        </Box>
+      </Center>
     </>
   );
 };
