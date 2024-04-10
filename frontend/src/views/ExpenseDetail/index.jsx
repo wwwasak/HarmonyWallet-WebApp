f74c71722efd022439b04341347b0057fe0eda81
Xingyan Hu <xhu819@aucklanduni.ac.nz>
2024-04-10 23:38:23 +1200

    added select to Income/Expense Pages

--- frontend/src/views/ExpenseDetail/index.jsx
+++ frontend/src/views/ExpenseDetail/index.jsx
@@ -1,7 +1,12 @@
-import { Text, Box, Center } from "@chakra-ui/react";
+import { Text, Box, Center, Select, Flex } from "@chakra-ui/react";
 import LineAndPieChartTabs from "./components/LineAndPieChartTabs";
+import { useState } from "react";
 
 export default function ExpenseDetail() {
+  const [selectedOption, setSelectedOption] = useState("NZD");
+
+  const handleChange = (event) => setSelectedOption(event.target.value);
+
   return (
     <Box>
       <Center>
@@ -9,6 +14,22 @@ export default function ExpenseDetail() {
           My Expense Details
         </Text>
       </Center>
+
+      <Flex justifyContent="flex-end">
+        <Select
+          width="auto"
+          minWidth="120px"
+          maxWidth="200px"
+          size="sm"
+          placeholder="Choose your currency"
+          value={selectedOption}
+          onChange={handleChange}
+        >
+          <option value="AUD">AUD</option>
+          <option value="NZD">NZD</option>
+          <option value="RMB">RMB</option>
+        </Select>
+      </Flex>
 
       <LineAndPieChartTabs />
     </Box>
