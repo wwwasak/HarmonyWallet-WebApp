c661eab31ee728e92e3509adcae830f5dfc01c02
shiyuzhou1013 <szho231@auckland.ac.nz>
2024-04-11 19:43:33 +1200

    calculator set up

--- frontend/src/views/Currency/RatesOverviewPage.jsx
+++ frontend/src/views/Currency/RatesOverviewPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import BaseCurrencySelector from "./components/BaseCurrencySelector";
+import Calculator from "./components/Calculator";
 
 const RatesOverviewPage = () => {
-  return <BaseCurrencySelector />;
+  return (
+    <>
+      <BaseCurrencySelector />
+      <Calculator />
+    </>
+  );
 };
 
 export default RatesOverviewPage;
