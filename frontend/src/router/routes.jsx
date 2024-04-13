460dc917caba54239c58f3ac93dde6ba9bca435d
Ian-Dzy <zdon070@aucklanduni.ac.nz>
2024-04-13 20:22:37 +1200

    built the router for detailed currency rates page

--- frontend/src/router/routes.jsx
+++ frontend/src/router/routes.jsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../views/Layout/LayoutPage";
 import ErrorPage from "../views/Layout/ErrorPage";
+import RatesDetailPage from "../views/Detail/RatesDetailPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
-    children: [], //All pages add inside here
+    children: [
+      {
+        path: "ratesDetail/:baseCurrency/:selectedCurrency",
+        element: <RatesDetailPage />,
+      },
+    ], //All pages add inside here
   },
 ]);
 
