ec44fe0ba6c935e2d5e5aaaab19d154fb260cc52
yang shi (yshi654) <yshi654@aucklanduni.ac.nz>
2024-04-17 00:58:03 +1200

    Create the recording page

--- frontend/src/router/routes.jsx
+++ frontend/src/router/routes.jsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../views/Layout/LayoutPage";
 import ErrorPage from "../views/Layout/ErrorPage";
+import RecordingPage from "../views/Record/RecordingPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
-    children: [], //All pages add inside here
+    children: [
+      {
+        path: "recording",
+        element: <RecordingPage />,
+      }
+    ], //All pages add inside here
   },
 ]);
 
