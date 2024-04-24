efa72554be2fdb8fe8a09844b96b46ea15b72c15
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-04-25 11:23:23 +1200

    small bug fix

--- frontend/src/views/Currency/components/CurrencySelector.jsx
+++ frontend/src/views/Currency/components/CurrencySelector.jsx
@@ -9,7 +9,7 @@ import {
   MenuList,
 } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import { CURRENCIES } from "../../../data/CURRENCIES.js";
+import { CURRENCIES } from "../../../data/CURRENCIES.JS";
 import React, { useState } from "react";
 
 const CurrencySelector = ({ baseCurrency, setBaseCurrency }) => {
