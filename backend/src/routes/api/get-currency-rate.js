d2361f0af2b22bc314c4b5f060e187f1cda9ebd2
Xingyan Hu <xhu819@aucklanduni.ac.nz>
2024-05-01 12:49:19 +1200

    add get-currency-rate api

--- backend/src/routes/api/get-currency-rate.js
+++ backend/src/routes/api/get-currency-rate.js
@@ -1,7 +1,7 @@
 import express from "express";
-import User from "../../models/user-schema.js";
 import Currency from "../../models/currency-schema.js";
 import { CURRENCIES } from "../../db/CURRENCIES.js";
+import { format } from "date-fns";
 
 const router = express.Router();
 
@@ -18,15 +18,41 @@ router.get("/", async (req, res) => {
       return res.status(404).send({ message: "Currency data not found." });
     }
 
-    res.status(200).json(currencyData);
+    if (!fromDate) {
+      return res.status(400).json({ message: "From date is required." });
+    }
+
+    const fromDateObj = new Date(fromDate);
+    const startDate = new Date(currencyData.start_date);
+    const endDate = new Date(currencyData.end_date);
+
+    if (fromDateObj < startDate || fromDateObj > endDate) {
+      return res
+        .status(400)
+        .json({ message: "From date is out of the range." });
+    }
+
+    const today = format(new Date(), "yyyy-MM-dd");
+    const filteredRates = new Map();
+
+    currencyData.rates.forEach((value, key) => {
+      if (key >= fromDate && key <= today) {
+        filteredRates.set(key, value);
+      }
+    });
+
+    const ratesObject = Object.fromEntries(filteredRates);
+
+    res.status(200).json({
+      currency: currencyData.currency,
+      rates: ratesObject,
+    });
   } catch (err) {
     console.error(err);
     res
       .status(500)
       .send({ message: "Server error while retrieving currency data." });
   }
-
-  console.log(req.query);
 });
 
 export default router;
