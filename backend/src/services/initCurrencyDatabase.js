08b6f41bc008272aa04c35398a3c1b2df297c411
Xingyan Hu <xhu819@aucklanduni.ac.nz>
2024-05-01 12:59:49 +1200

    modify init-db, improve efficiency of initialize currency database

--- backend/src/services/initCurrencyDatabase.js
+++ backend/src/services/initCurrencyDatabase.js
@@ -6,11 +6,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const API_BASE_URL = process.env.RELATED_CURRENCY_STRING;
-const fromDate = getSevenDaysAgoFormatted();
+const fromDate = getNDaysAgoFormatted(800);
 
-function getSevenDaysAgoFormatted() {
-  const sevenDaysAgo = subDays(new Date(), 7);
-  return format(sevenDaysAgo, "yyyy-MM-dd");
+function getNDaysAgoFormatted(n) {
+  const nDaysAgo = subDays(new Date(), n);
+  return format(nDaysAgo, "yyyy-MM-dd");
 }
 
 async function fetchCurrency(currency) {
@@ -19,21 +19,23 @@ async function fetchCurrency(currency) {
   return response.data;
 }
 
+async function fetchAllCurrencies() {
+  const promises = CURRENCIES.map((currency) => fetchCurrency(currency));
+  return Promise.all(promises);
+}
+
 async function initCurrencyDatabase() {
   try {
-    for (const currency of CURRENCIES) {
-      const data = await fetchCurrency(currency);
+    const results = await fetchAllCurrencies();
+    const currenciesData = results.map((data, index) => ({
+      currency: CURRENCIES[index],
+      amount: data.amount,
+      start_date: new Date(data.start_date),
+      end_date: new Date(data.end_date),
+      rates: data.rates,
+    }));
 
-      const newCurrencyData = new Currency({
-        currency,
-        amount: data.amount,
-        start_date: new Date(data.start_date),
-        end_date: new Date(data.end_date),
-        rates: data.rates,
-      });
-
-      await newCurrencyData.save();
-    }
+    await Currency.insertMany(currenciesData);
     console.log("Currency data initialized successfully.");
   } catch (error) {
     console.error("Failed to fetch or save currency data:", error);
