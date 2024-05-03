67c3fb12c50e63c3f31705c834763fb72cf813e2
Xingyan Hu <xhu819@aucklanduni.ac.nz>
2024-05-03 22:47:53 +1200

    added change password api

--- backend/src/routes/api/change-password.js
+++ backend/src/routes/api/change-password.js
@@ -1,25 +1,23 @@
 import express from "express";
 import User from "../../models/user-schema.js";
 import bcrypt from "bcryptjs";
+import { authenticateToken } from "../../middleware/authenticateToken.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const { username, password, security_question, question_answer } = req.body;
+router.post("/update-password", authenticateToken, async (req, res) => {
+  const { password } = req.body;
+  const userId = req.user.userId;
+
+  if (!password) {
+    return res.status(400).send({ message: "Password is required" });
+  }
 
   try {
-    const user = await User.findOne({
-      username: username,
-      security_question: security_question,
-      question_answer: question_answer,
-    });
-    if (!user) {
-      return res.status(401).send({ message: "Security info does not match" });
-    }
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const result = await User.findOneAndUpdate(
-      { _id: user._id },
+    const result = await User.findByIdAndUpdate(
+      userId,
       { $set: { password: hashedPassword } },
       { new: true }
     );
@@ -27,9 +25,10 @@ router.post("/", async (req, res) => {
     if (result) {
       return res.status(200).send({ message: "Password updated successfully" });
     } else {
-      return res.status(400).send({ message: "Password update failed" });
+      return res.status(404).send({ message: "User not found" });
     }
   } catch (error) {
+    console.error("Failed to update password:", error);
     return res.status(500).send({ message: "Server error" });
   }
 });
