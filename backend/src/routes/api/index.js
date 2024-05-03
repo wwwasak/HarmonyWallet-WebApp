833b1daf36ca1b37c30cc9555060b9b4fd36610c
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-05-03 16:12:21 +1200

    removed daily rate

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import dailyRates from "./dailyRates.js";
 import checkUsername from "./check-username.js";
 import checkUsernameQuestion from "./check-username-question.js";
 import changePassowrd from "./change-password.js";
@@ -13,7 +12,6 @@ import getUserInfo from "./get-user-info.js";
 
 const router = express.Router();
 
-router.use("/dailyRates", dailyRates);
 router.use("/check-username", checkUsername);
 router.use("/check-username-question", checkUsernameQuestion);
 router.use("/change-password", changePassowrd);
--- backend/src/utils/API_KEY.js
+++
@@ -1,1 +0,0 @@
-module.exports.API_KEY = "61ef9148f656465a89126e0c0bedf31b";
