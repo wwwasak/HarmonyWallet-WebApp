87ed02eaa46b6431551bf1e6aacc277ea690eb7d
Xingyan Hu <xhu819@aucklanduni.ac.nz>
2024-05-03 17:33:28 +1200

    modified exchange page

--- frontend/src/views/ExchangeRecordDetail/index.jsx
+++ frontend/src/views/ExchangeRecordDetail/index.jsx
@@ -1,25 +1,39 @@
-import { Box, Text, Center, Button, Flex } from "@chakra-ui/react";
+import { Box, Text, Center, Button, Flex, Spinner } from "@chakra-ui/react";
 import ExchangeRecordChart from "./components/ExchangeRecordChart";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import { format, subDays, subMonths } from "date-fns";
-
-const oneWeekAgo = format(subDays(new Date(), 6), "yyyy-MM-dd");
-const twoWeeksAgo = format(subDays(new Date(), 13), "yyyy-MM-dd");
-const oneMonthAgo = format(subMonths(new Date(), 1), "yyyy-MM-dd");
-const oneYearAgo = format(subDays(new Date(), 364), "yyyy-MM-dd");
 
 export default function ExchangeRecordDetail() {
   const [weeklyData, setWeeklyData] = useState(null);
   const [fortnightlyData, setFortnightlyData] = useState(null);
   const [monthlyData, setMonthlyData] = useState(null);
   const [yearlyData, setYearlyData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const oneWeekAgo = useMemo(
+    () => format(subDays(new Date(), 6), "yyyy-MM-dd"),
+    []
+  );
+  const twoWeeksAgo = useMemo(
+    () => format(subDays(new Date(), 13), "yyyy-MM-dd"),
+    []
+  );
+  const oneMonthAgo = useMemo(
+    () => format(subMonths(new Date(), 1), "yyyy-MM-dd"),
+    []
+  );
+  const oneYearAgo = useMemo(
+    () => format(subDays(new Date(), 364), "yyyy-MM-dd"),
+    []
+  );
 
   useEffect(() => {
     fetchData(oneWeekAgo, setWeeklyData);
-  }, []);
+  }, [oneWeekAgo]);
 
   const fetchData = async (fromDate, setData) => {
+    setIsLoading(true);
     const url = import.meta.env.VITE_GET_EXCHANGES_URL;
     const authToken = localStorage.getItem("authToken");
     const body = {
@@ -36,27 +50,32 @@ export default function ExchangeRecordDetail() {
     } catch (error) {
       console.error("Error fetching user info:", error);
     }
+    setIsLoading(false);
   };
 
   const handleClickWeekly = () => {
+    console.log(weeklyData);
     if (weeklyData === null) {
       fetchData(oneWeekAgo, setWeeklyData);
     }
   };
 
   const handleClickFortnightly = () => {
+    console.log(fortnightlyData);
     if (fortnightlyData === null) {
       fetchData(twoWeeksAgo, setFortnightlyData);
     }
   };
 
   const handleClickMonthly = () => {
+    console.log(monthlyData);
     if (monthlyData === null) {
       fetchData(oneMonthAgo, setMonthlyData);
     }
   };
 
   const handleClickYearly = () => {
+    console.log(yearlyData);
     if (yearlyData === null) {
       fetchData(oneYearAgo, setYearlyData);
     }
@@ -80,14 +99,20 @@ export default function ExchangeRecordDetail() {
           </Flex>
         </Box>
 
-        <Box bg="green.200" minH={400} minW={500}>
-          <ExchangeRecordChart
-            weeklyData={weeklyData}
-            fortnightlyData={fortnightlyData}
-            monthlyData={monthlyData}
-            yearlyData={yearlyData}
-          />
-        </Box>
+        {isLoading ? (
+          <Center height="400px">
+            <Spinner size="xl" />
+          </Center>
+        ) : (
+          <Box bg="green.200" minH={400} minW={500}>
+            <ExchangeRecordChart
+              weeklyData={weeklyData}
+              fortnightlyData={fortnightlyData}
+              monthlyData={monthlyData}
+              yearlyData={yearlyData}
+            />
+          </Box>
+        )}
       </Box>
     </>
   );
